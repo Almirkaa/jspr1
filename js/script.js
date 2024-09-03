7e1ab3a845e1cc888375f7932a235f8559a42a36
Almirkaa <almigarifullina@gmail.com>
2024-09-03 16:50:38 +0400

    Практика1

---
+++ js/script.js
@@ -0,0 +1,12 @@
+'use strict';
+let nam = prompt('Введите ваше имя', 'имя');
+let fname = prompt('Введите вашу фамилию', 'фамилия');
+let otch = prompt('Введите ваше Отчество', 'Отчество');
+
+alert(fname);
+alert(nam);
+alert(otch);
+
+
+let isTrue = confirm("Данные верны?");
+alert(isTrue);
